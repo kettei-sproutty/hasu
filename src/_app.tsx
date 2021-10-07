1e696cce53464d55bf6605530f48a9fac6a90332
Alessio Marchi <ketteisproutty@gmail.com>
2021-10-07 02:16:38 +0200

    feat: create header placeholder

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import Header from './components/layout/header'
 import LoadingComponent from './components/loading'
 import AppContext from './context'
 import Routes from './router'
+import './styles/index.scss'
 
 const MyApp: React.FC = () => {
     return (
         <AppContext>
            <LoadingComponent />
+           <Header />
            <Routes />
         </AppContext>
     )
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import logo from '../../public/icons/32.webp'
+
+const Header: React.FC = () => {
+  const history = useHistory();
+
+  const navigateToHome = (e: React.MouseEvent) => {
+    e.preventDefault()
+    history.push('/')
+  }
+
+  return (
+    <nav className="header">
+      <div className="inline-flex">
+        <a className="header-logo" href='/' onClick={navigateToHome}>
+          <span className="sr-only">Go to Home</span> 
+          <div className="block">
+            <img src={logo} />
+          </div>
+        </a>
+      </div>
+      <div className="flex-initial">
+        
+      </div>
+    </nav>
+  )
+}
+
+export default Header
