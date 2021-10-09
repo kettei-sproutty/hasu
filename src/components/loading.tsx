b070a15027432fab49b627d67c53b8fa0e3ee7bb
Alessio Marchi <ketteisproutty@gmail.com>
2021-10-09 04:29:44 +0200

    feat: add logo component

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import useLoading from '../context/loading/useLoading'
+import SmallLogo from './layout/logo-small'
 
 const LoadingComponent: React.FC = () => {
   const [loading] = useLoading()
 
   if (!loading) return null
 
-  return <div className="absolute z-50 bg-pink-400 size-full" />
+  return (
+    <div className="absolute z-50 bg-gray-200 size-full">
+      <SmallLogo
+        className={
+          'absolute inset-1/2 transform -translate-x-1/2 -translate-y-1/2 animate-pulse'
+        }
+        width={64}
+        height={64}
+      />
+    </div>
+  )
 }
 
 export default LoadingComponent
