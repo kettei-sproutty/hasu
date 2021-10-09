12ea1bd9347a08b2190c0b781eefd973c66ebe0b
Alessio Marchi <ketteisproutty@gmail.com>
2021-10-09 04:32:05 +0200

    feat: update home and history pages

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
 const HistoryPage: React.FC = () => {
-
-    return (
-        <div className="page">
-            <span>History</span>
-        </div>
-    )
+  return (
+    <div className="page">
+      History
+    </div>
+  )
 }
 
 export default HistoryPage
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useHistory } from 'react-router'
+import MiniGallery from '../components/gallery/mini'
 import getPageRoute from '../context/routes/get-route'
 
 const HomePage: React.FC = () => {
@@ -11,9 +12,17 @@ const HomePage: React.FC = () => {
         history.push(route)
     }
 
+    const operas = [
+        { image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1574895616l/43824485._SX318_.jpg', title: 'The Beginning After the End' },
+        { image: 'https://images-na.ssl-images-amazon.com/images/I/71gnuYUWeTL.jpg', title: 'Solo Leveling' },
+        { image: 'https://www.animeclick.it/immagini/anime/Tate_no_Yuusha_no_Nariagari/cover/Tate_no_Yuusha_no_Nariagari-cover.jpg', title: 'Tate no Yuusha' },
+    ]
+
     return (
         <div className="page">
-            <button className="button button__primary" onClick={navigateToLogin}>
+            <MiniGallery page={'history'} medias={operas} title={'history'} />
+            <MiniGallery page={'login'} medias={operas} title={'favorites'} />
+            <button className="hidden button button__primary" onClick={navigateToLogin}>
                 <span className="not-sr-only">Auth</span>
             </button>
         </div>
